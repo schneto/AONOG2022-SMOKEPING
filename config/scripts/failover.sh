df1464009bb0046f5853494514d142c088c9b263
schneto <schumacherneto@gmail.com>
2023-03-14 11:38:46 +0100

    Create failover.sh

---
+++ config/scripts/failover.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Run your failover script here
+# For example, to switch to a backup server:
+echo "Failover script executed."
+ping 196.216.48.147
+#ssh schumacher.neto@196.216.48.147
